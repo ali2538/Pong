6e3982ca9cfdd47a1e90821597caf920ad70e5cb
Ali Hadizadeh <ali@hadizadeh.com>
2024-04-22 20:29:12 -0400

    added a prompt and ball's speed increments

--- Ball.py
+++ Ball.py
@@ -19,7 +19,7 @@ class Ball(pygame.sprite.Sprite):
         self.pong_ball = pygame.Rect(
             self.position_x, self.position_y, self.width, self.height
         )
-        self.speed = 5
+        self.speed = 3
         self.angle_in_degrees = random.choice(
             [
                 ang
@@ -35,6 +35,9 @@ class Ball(pygame.sprite.Sprite):
     def update_angle(self):
         self.angle_in_radian = math.pi * self.angle_in_degrees / 180
 
+    def increase_speed(self, new_speed=0.1):
+        self.speed += new_speed
+
     def check_position(
         self, left_pad, right_pad, left_side_score_update, right_side_score_update
     ) -> bool:
@@ -43,9 +46,15 @@ class Ball(pygame.sprite.Sprite):
         ):
             self.angle_in_degrees -= 90
             self.update_angle()
+            self.increase_speed()
+            print(f"speed is {self.speed}")
         if not self.screen_area.contains(self.pong_ball):
-            if (self.pong_ball.top < 5.0 or self.pong_ball.bottom > 590.0) and (
-                (not self.pong_ball.left < 10.0) and (not self.pong_ball.right > 1000.0)
+            if (
+                self.pong_ball.top < 2
+                or self.pong_ball.bottom > constants.SCREEN_HEIGHT - 2
+            ) and (
+                (not self.pong_ball.left < 10)
+                and (not self.pong_ball.right > constants.SCREEN_WIDTH - 10)
             ):
                 self.angle_in_degrees = -self.angle_in_degrees
                 self.update_angle()
--- PongMain.py
+++ PongMain.py
@@ -31,6 +31,17 @@ def create_update_score(left_player_score, right_player_score):
     }
 
 
+def start_game_prompt():
+    font = pygame.font.SysFont("Times New Roman", 48)
+    text = font.render(
+        "Press Enter to Begin", True, constants.SCORE_TEXT_COLOR, constants.BLACK
+    )
+    text_rect = text.get_rect()
+    text_rect.top = constants.SCREEN_HEIGHT / 2
+    text_rect.left = constants.SCREEN_WIDTH / 2 - text_rect.width / 2
+    return (text, text_rect)
+
+
 def play_pong():
     still_playing = True
 
@@ -42,6 +53,7 @@ def play_pong():
     new_ball = True
     pong_ball = None
     new_game = True
+    start_game = False
     pad1 = Paddle(constants.LEFT_SIDE_PAD, "player1")
     player_on_left = Player("player1", constants.LEFT_SIDE_PAD)
     pad2 = Paddle(constants.RIGHT_SIDE_PAD, "player2")
@@ -49,41 +61,49 @@ def play_pong():
 
     while still_playing:
         screen.fill(color=constants.BLACK)
-        scores_list = create_update_score(player_on_left.score, player_on_right.score)
-        screen.blit(scores_list["left"][0], scores_list["left"][1])
-        screen.blit(scores_list["right"][0], scores_list["right"][1])
-        if new_ball:
-            pong_ball = Ball(screen)
-            new_ball = False
-        keypressed = pygame.key.get_pressed()
-        if keypressed[pygame.K_UP]:
-            pad2.move_up()
-        if keypressed[pygame.K_DOWN]:
-            pad2.move_down()
-
-        if keypressed[pygame.K_w]:
-            pad1.move_up()
-        if keypressed[pygame.K_s]:
-            pad1.move_down()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                still_playing = False
-
         # draw a line in the middle of the screen
         for ht in range(0, 600, 41):
             dashed_line_dot = pygame.Rect(505, ht, 10, 20)
             pygame.draw.rect(screen, constants.DASHED_LINE_COLOR, dashed_line_dot)
-        pad1.update(screen)
-        pad2.update(screen)
-        pong_ball.move(screen)
-        if pong_ball.check_position(
-            left_pad=pad1,
-            right_pad=pad2,
-            left_side_score_update=player_on_left.score_update,
-            right_side_score_update=player_on_right.score_update,
-        ):
-            new_ball = True
-            del pong_ball
+        scores_list = create_update_score(player_on_left.score, player_on_right.score)
+        screen.blit(scores_list["left"][0], scores_list["left"][1])
+        screen.blit(scores_list["right"][0], scores_list["right"][1])
+        if not start_game:
+            initial_prompt = start_game_prompt()
+            screen.blit(initial_prompt[0], initial_prompt[1])
+        if start_game:
+            if new_ball:
+                pong_ball = Ball(screen)
+                new_ball = False
+            keypressed = pygame.key.get_pressed()
+            if keypressed[pygame.K_UP]:
+                pad2.move_up()
+            if keypressed[pygame.K_DOWN]:
+                pad2.move_down()
+
+            if keypressed[pygame.K_w]:
+                pad1.move_up()
+            if keypressed[pygame.K_s]:
+                pad1.move_down()
+            pad1.update(screen)
+            pad2.update(screen)
+            pong_ball.move(screen)
+            if pong_ball.check_position(
+                left_pad=pad1,
+                right_pad=pad2,
+                left_side_score_update=player_on_left.score_update,
+                right_side_score_update=player_on_right.score_update,
+            ):
+                new_ball = True
+                del pong_ball
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                still_playing = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    if not start_game:
+                        start_game = True
+
         pygame.display.flip()
         clock.tick(60)
 
